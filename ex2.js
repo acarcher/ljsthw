f10c979e93395aaba436c6e1d963916c4d4781b9
Alexander Archer <aca3936@gmail.com>
2019-06-20 13:36:43 -0400

    ex2

---
+++ ex2.js
@@ -0,0 +1,14 @@
+// This is a comment, everything after is ignored.
+
+console.log("I am Code");
+
+/* This is also a comment. You use this for
+ * longer things that you want to say.
+ */
+console.log("So am I");
+
+// The code inside the next comment will not run:
+
+/*
+console.log("I will not run.");
+*/
