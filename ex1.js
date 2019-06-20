f20f61926fe2731041d3bde9b2e12d209f39599d
Alexander Archer <aca3936@gmail.com>
2019-06-20 13:36:32 -0400

    ex1

---
+++ ex1.js
@@ -0,0 +1,6 @@
+console.log("You are writing text to your screen.")
+console.log("This is called 'standard output'.")
+console.log("This stuff in the () is a string.")
+console.log("Start a string with a double-quote.")
+console.log("Write what you want.")
+console.log("Then end with a double-quote.")
