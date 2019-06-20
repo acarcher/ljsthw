55fe2d15d1999403752b099c5c1d0b4b2a9293fb
Alexander Archer <aca3936@gmail.com>
2019-06-20 15:23:37 -0400

    ex4

---
+++ ex3.js
@@ -0,0 +1,8 @@
+// You can give multiple things for console.log to print.
+// Separate each thing with a comma.
+console.log("Name:", 'Zed A. Shaw');
+console.log("Age:", 43);
+console.log("Feet Tall:", Math.round(74 / 12));
+console.log("And Inches:", 74 - (Math.round(74 / 12) * 12));
+console.log("Age * Height:", 43 * 74);
+console.log("Age * Feet:", 43 * Math.round(74 / 12));
