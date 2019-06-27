88123269a0a8c43314886a48c4f903d48118eb08
Alexander Archer <aca3936@gmail.com>
2019-06-27 19:07:59 -0400

    ex24 refactor

--- ex24.js
+++ ex24.js
@@ -1,8 +1,12 @@
 const readline = require('readline-sync');
 
 class Game {
-    constructor () {
+    constructor (rooms) {
         this.hp = Math.floor(Math.random() * 10) + 1;
+        this.rooms = {}
+        for(let room of rooms) {
+            this.rooms[room.name] = room;
+        }
     }
 
     say(prompt) {
@@ -23,17 +27,23 @@ class Game {
         }
     }
 
-    addRoom(room) {
-        this[room.name] = room;
-        room.game = this;
+    go(where) {
+        let room = this.rooms[where];
+        return room.enter(this);
     }
 
     play(name) {
-        this[name].enter();
+        let next_room = this.go(name);
+        while(next_room) {
+            next_room = this.go(next_room);
+        }
     }
 
     hit(amount) {
         this.hp -= amount;
+        if(this.hp <= 0) {
+            this.die("You died from internal injuries.");
+        }
     }
 }
 
@@ -48,57 +58,63 @@ class Room {
 }
 
 class Door extends Room {
-    enter() {
+    enter(game) {
         // they have to open the door to get the gold
         // what kind of puzzle will they solve?
     }
 }
 
 class Spider extends Room {
-    enter() {
+    enter(game) {
         // they enter here, and the spider takes 10 hit points
         // if they live then they can run away
     }
 }
 
 class Gold extends Room {
-    enter() {
+    enter(game) {
         //end of the game they win if they get the gold
     }
 }
 
 class Rope extends Room {
-    enter() {
+    enter(game) {
         // they are at the bottom of the well
+        game.say("You are at the bottom of the well.");
         // they can go through the door to the gold
+        return "well";
         // or go take a wrong turn to the spider
     }
 }
 
 class Well extends Room {
-    enter(){
-        this.game.say("You are walking through the woods and see a well.");
-        this.game.say("Walking up to it and looking down you see a shiny thing at the bottom.");
-        let next = this.game.ask("What do you do?");
+
+    enter(game) {
+
+        game.say("You are walking through the woods and see a well.");
+        game.say("Walking up to it and looking down you see a shiny thing at the bottom.");
+        let next = game.ask("What do you do?");
 
         if(next === "climb") {
-            this.game.say("You climb down the rope.");
-            this.game.rope.enter();
+            game.say("You climb down the rope.");
+            return "rope";
         } else if(next === "jump") {
-            this.game.say("Yikes! Let's see if you survive!");
-            this.game.hit(5);
-            this.game.rope.enter();
+            game.say("Yikes! Let's see if you survive!");
+            game.hit(5);
+            return "rope";
         } else {
-            this.game.say("You can't do that here.");
-            this.game.well.enter();
+            game.say("You can't do that here.");
+            return "well";
         }
     }
 }
 
-let game = new Game();
-game.addRoom(new Well("well"));
-game.addRoom(new Rope("rope"));
-game.addRoom(new Gold("gold"));
-game.addRoom(new Spider("spider"));
-game.addRoom(new Door("door"));
+let rooms = [
+    new Well("well"),
+    new Rope("rope"),
+    new Gold("gold"),
+    new Spider("spider"),
+    new Door("door")
+]
+let game = new Game(rooms);
 game.play("well");
